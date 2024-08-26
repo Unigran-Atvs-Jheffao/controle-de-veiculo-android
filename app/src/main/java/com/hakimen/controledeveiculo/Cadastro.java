4475ec3a862add0c7859c35c36934e638c4b6ac1
JheffersonMarques <jheffersonmarques@outlook.com>
2024-08-26 15:42:39 -0400

    Added more fields to the Vehicle Object

--- app/src/main/java/com/hakimen/controledeveiculo/Cadastro.java
+++ app/src/main/java/com/hakimen/controledeveiculo/Cadastro.java
@@ -10,6 +10,9 @@ public class Cadastro extends AppCompatActivity {
     Button btnCadastrar;
     Button btnVoltar;
     EditText edtCadastro;
+    EditText edtMarca;
+    EditText edtModelo;
+    EditText edtPlaca;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +23,21 @@ public class Cadastro extends AppCompatActivity {
         btnVoltar = findViewById(R.id.btn_cancelar);
 
         edtCadastro = findViewById(R.id.edit_txt_vehicle_name);
+        edtMarca = findViewById(R.id.edit_txt_vehicle_brand);
+        edtModelo = findViewById(R.id.edit_txt_vehicle_model);
+        edtPlaca = findViewById(R.id.edit_txt_vehicle_license_plate);
 
         btnCadastrar.setOnClickListener(v -> {
-            Dados.VEICULOS.add(new Dados.Veiculo(edtCadastro.getText().toString()));
+            Dados.VEICULOS.add(new Dados.Veiculo()
+                    .setNome(edtCadastro.getText().toString())
+                    .setMarca(edtMarca.getText().toString())
+                    .setPlaca(edtPlaca.getText().toString())
+                    .setModelo(edtModelo.getText().toString()));
             onBackPressed();
         });
-        btnVoltar.setOnClickListener(v -> {onBackPressed();});
+        btnVoltar.setOnClickListener(v -> {
+            onBackPressed();
+        });
 
     }
 }
--- app/src/main/java/com/hakimen/controledeveiculo/Dados.java
+++ app/src/main/java/com/hakimen/controledeveiculo/Dados.java
@@ -9,21 +9,53 @@ import java.util.List;
 public class Dados {
     public static final List<Veiculo> VEICULOS = new ArrayList<>();
 
-    static {
-    }
-
     static class Veiculo{
         String nome;
+        String marca;
+        String modelo;
+        String placa;
 
-        public Veiculo(String nome) {
+        public String getNome() {
+            return nome;
+        }
+
+        public Veiculo setNome(String nome) {
             this.nome = nome;
+            return this;
+        }
+
+        public String getMarca() {
+            return marca;
+        }
+
+        public Veiculo setMarca(String marca) {
+            this.marca = marca;
+            return this;
+        }
+
+        public String getModelo() {
+            return modelo;
+        }
+
+        public Veiculo setModelo(String modelo) {
+            this.modelo = modelo;
+            return this;
+        }
+
+        public String getPlaca() {
+            return placa;
+        }
+
+        public Veiculo setPlaca(String placa) {
+            this.placa = placa;
+            return this;
         }
 
         @NonNull
         @NotNull
         @Override
         public String toString() {
-            return nome;
+            return "Nome: " + nome + "\n" + "Marca: " + marca + "\n" + "Modelo: " + modelo + "\n" + "Placa: " + placa;
         }
     }
 }
